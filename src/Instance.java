d92c8dd234bd4933ec99f91beb351ff60f5dbd7e
Abraham Alfred <53820130+Abraham-Alfred@users.noreply.github.com>
2021-04-18 17:16:13 +1200

    Finished code. Some comments missing.

--- src/Instance.java
+++ src/Instance.java
@@ -1,25 +1,57 @@
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
+/**
+ * Helper class for decision tree which
+ * has the categories and the values of
+ * the data files.
+ *
+ * @author alfredabra 300509598
+ **/
 public class Instance {
 
-    private String category;
-    private List<Boolean> vals;
+    private final String category;
+    private final List<Boolean> vals;
 
+    /**
+     * Constructor for the Instance class.
+     *
+     * @param bool bool
+     * @param cat cat
+     **/
     public Instance(String cat, List<Boolean> bool){
         category = cat;
         vals = bool;
     }
 
+    // ============================================================
+    // Getters, setters and toString.
+    // ============================================================
+
+    /**
+     * Getter for val.
+     *
+     * @param index index
+     * @return vals index
+     **/
     public boolean getVal(int index){
         return vals.get(index);
     }
 
+    /**
+     * Getter for Category.
+     *
+     * @return category category
+     **/
     public String getCategory(){
         return category;
     }
 
+    /**
+     * toString method which converts the vals
+     * to true and false.
+     *
+     * @return ans ans
+     **/
     public String toString(){
         StringBuilder ans = new StringBuilder(category);
         ans.append(" ");
